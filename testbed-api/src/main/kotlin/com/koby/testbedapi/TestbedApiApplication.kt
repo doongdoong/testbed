0fc07004e69c1a4e28147b8bbf302bfb0d767172
doongdoong <ksy940822@naver.com>
2024-07-30 00:24:00 +0900

    미사용 import문 제거

--- testbed-api/src/main/kotlin/com/koby/testbedapi/TestbedApiApplication.kt
+++ testbed-api/src/main/kotlin/com/koby/testbedapi/TestbedApiApplication.kt
@@ -2,7 +2,6 @@ package com.koby.testbedapi
 
 import com.koby.testbeddomain.DomainConfigurationLoader
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
